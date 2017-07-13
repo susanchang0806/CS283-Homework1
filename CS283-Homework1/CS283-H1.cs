9c9fbff7fcd8f5002988ff610e0749caf0e42734
susanchang0806 <s79536117@gmail.com>
2017-07-13 14:15:28 +0800

    homework
    homework

---
+++ CS283-Homework1/CS283-H1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PokemonLib;
+
+namespace CS283H1{
+    class Program    {
+        static void Main(string[] args){
+            Pokemon aPokemon = new Pokemon();
+        }
+    }
+}
