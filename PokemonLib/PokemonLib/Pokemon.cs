444e1969bfaca9170f10f873568148c87a65bc4a
susanchang0806 <s79536117@gmail.com>
2017-07-14 10:24:49 +0800

    homework_r1
    homework_r1

---
+++ PokemonLib/PokemonLib/Pokemon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonLib
+{
+    public class Pokemon
+    {
+        public string pname;
+        public string gender;
+        public int hp_max;
+        public int hp_now;
+        public int pweight;
+        public int pheight;
+        public string pattribute;
+        public int stardust;
+        public int candy;
+        public string skill1;
+        public int skill1_power;
+        public string skill2;
+        public int skill2_power;
+
+    }
+}
